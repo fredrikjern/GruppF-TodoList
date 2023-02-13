c62aa64d9994583c5bbf9d4ca18f2f2bf73cda50
LeoM <leobmagndahl@gmail.com>
2023-02-13 13:08:45 +0100

    Added delete function

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-async function deleteBtn(item) {
+/*
+This function takes in the a list and an item and deletes the selected item.
+Use this in the context of the delete button appended to each list item.
+*/
+async function deleteFunction(currentList, item) {
   const res = await fetch(`${API_BASE}lists/${currentList}/items/${item._id}`, {
     method: "DELETE",
   }); // deletar objekt med _id.
-  const { list } = await res.json(); // Hämtar den nya listan som där objektet är borttaget.
+  let { list } = await res.json(); // Hämtar den nya listan som där objektet är borttaget.
+  return list;
 }
