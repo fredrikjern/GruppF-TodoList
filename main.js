b93477d0aab4ebc6f7106b20bbcb6e44e1711a90
LeoM <leobmagndahl@gmail.com>
2023-02-13 14:41:32 +0100

    added getList function

--- main.js
+++ main.js
@@ -46,10 +46,6 @@ async function deleteFunction(currentList, item) {
   let { list } = await res.json(); // Hämtar den nya listan som där objektet är borttaget.
   return list;
 }
-
-let buyListInput = document.querySelector("#buy-list-input");
-let buyList = document.querySelector("#buy-list");
-
 // Api post funtion that adds items into buy list
 async function apiPost(str) {
   const res = await fetch(
@@ -68,10 +64,16 @@ async function apiPost(str) {
   //const { buyList } = await res.json();
 }
 
-async function apiGet(list) {
+async function apiGet(listID) {
   const res = await fetch(
-    `https://nackademin-item-tracker.herokuapp.com/listsearch?listname=${list}`
+    `https://nackademin-item-tracker.herokuapp.com/list/:${listID}`
   );
   const data = await res.json();
   return data;
 }
+let buyListID = apiGet("63ea106e843a53f2e4b457f3");
+console.log(buyListID);
+let inventoryListId = apiGet("63ea107d843a53f2e4b457f4");
+console.log(inventoryListId);
+
+//-------------------------------------------------------
