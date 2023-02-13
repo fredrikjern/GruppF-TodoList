9fab6be0d5fc82b9c8dfcd77112520d4afc4e8c9
SofiaLoven <sofia.loven93@gmail.com>
2023-02-13 13:26:25 +0100

    push

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-let buyListInput = document.querySelector(".buy-list-input");
+let buyListInput = document.querySelector("#buy-list-input");
+let buyList = document.querySelector("#buy-list");
+
+async function apiPost(str){
+    const res = await fetch(
+        `https://nackademin-item-tracker.herokuapp.com/lists/GruppF-UserName-Buy/items`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: str
+          }),
+        }
+      );
+      console.log("Hej");
+      //const { buyList } = await res.json();
+};
+
+buyListInput.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    let input = document.querySelector("#buy-list-input> input").value;
+    apiPost(input);
+});
