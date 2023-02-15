477cd8d0ab66ce7958f8668a234c9b42a0227e16
LeoM <leobmagndahl@gmail.com>
2023-02-15 10:51:26 +0100

    Working on apiPost function

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
+const API_BASE = "https://nackademin-item-tracker.herokuapp.com/"; //Den delen av API:urlen som är samma för alla anrop
+
 let buyListInput = document.querySelector("#buy-list-input");
 let buyList = document.querySelector("#buy-list");
+let shoppingField = document.querySelector("#shoppingField");
+let homeField = document.querySelector("#homeField");
+let shopButton = document.querySelector("#shopButton");
+let homeButton = document.querySelector("#homeButton");
+let currentList = "";
 
-// Api post funtion that adds items into buy list
-async function apiPost(str) {
+async function apiGet(listID) {
   const res = await fetch(
-    `https://nackademin-item-tracker.herokuapp.com/lists/GruppF-UserName-Buy/items`,
+    `https://nackademin-item-tracker.herokuapp.com/list/:${listID}`
+  );
+  const data = await res.json();
+  return data;
+}
+// let buyListData = apiGet("63ea106e843a53f2e4b457f3");
+console.log(buyListData);
+// let inventoryListData = apiGet("63ea107d843a53f2e4b457f4");
+console.log(inventoryListData);
+// Api post funtion that adds items into buy list
+// ID till input fältet #shoppingField och #homeField
+// ID till knappen för input fältet #shopButton och #homeButton
+const shopValue = shoppingField.value;
+shoppingField.addEventListener("sumbit", function (e) {
+  e.preventDefault();
+  apiPost("63ea106e843a53f2e4b457f3");
+});
+async function apiPost(listID) {
+  const res = await fetch(
+    `https://nackademin-item-tracker.herokuapp.com/lists/${listID}/items`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        title: str,
+        title: shopValue,
       }),
     }
   );
+  const { list } = await res.json();
+  console.log(list.itemList);
+  // drawItems(list.itemList);
   console.log("Hej");
   //const { buyList } = await res.json();
 }
-
+/*
 // Submit eventListener on buy item form
 buyListInput.addEventListener("submit", (event) => {
   event.preventDefault();
   let input = document.querySelector("#buy-list-input> input").value;
   apiPost(input);
 });
-
+*/
 // Takes all items and prits it to desired list
 function printToList(items, list) {
   list.innerHTML = "";
@@ -46,17 +74,5 @@ async function deleteFunction(currentList, item) {
   let { list } = await res.json(); // Hämtar den nya listan som där objektet är borttaget.
   return list;
 }
-
-async function apiGet(listID) {
-  const res = await fetch(
-    `https://nackademin-item-tracker.herokuapp.com/list/:${listID}`
-  );
-  const data = await res.json();
-  return data;
-}
-let buyListData = apiGet("63ea106e843a53f2e4b457f3");
-console.log(buyListData);
-let inventoryListData = apiGet("63ea107d843a53f2e4b457f4");
-console.log(inventoryListData);
 
 //-------------------------------------------------------
