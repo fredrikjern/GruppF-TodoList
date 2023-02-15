e657b50cd5a860901cf289d5333bfd4276a54b71
Martin <martinbirgell@gmail.com>
2023-02-15 10:54:40 +0100

    created testvariable

--- main.js
+++ main.js
@@ -2,6 +2,25 @@ const buyList = document.getElementById("buy-list")
 
 
 
+// Function that takes buyList or inventoryList and then calls the write function.
+async function apiGet(listName) {
+  let listID
+  if (listName === "buyList") {
+    listID = "63ea106e843a53f2e4b457f3"
+  } else if (listName === "inventoryList") {
+    listID = "63ea107d843a53f2e4b457f4"
+  } else {
+    return console.error("ApiGet function need to know if it is 'buylist' or 'inventoryList'!")
+  }
+  const res = await fetch(
+    `https://nackademin-item-tracker.herokuapp.com/lists/${listID}`
+    );
+  const data = await res.json();
+  console.log(data.itemList) //TODO Change this line to a draw function
+}
+
+
+
 // Api post funtion that adds items into buy list
 async function apiPost(str) {
   const res = await fetch(
@@ -21,7 +40,12 @@ async function apiPost(str) {
 }
 
 
-
+const myArr = [
+  {
+    itemList: ["sak1", "sak2", "sak3"],
+    _id: "dsgdg32432fdf",
+  }
+]
 // Takes all items and prits it to desired list
 function printToList(items, list) {
   list.innerHTML = "";
@@ -41,24 +65,6 @@ async function deleteFunction(currentList, item) {
   }); // deletar objekt med _id.
   let { list } = await res.json(); // Hämtar den nya listan som där objektet är borttaget.
   return list;
-}
-
-
-// Function that takes buyList or inventoryList and then calls the write function.
-async function apiGet(listName) {
-  let listID
-  if (listName === "buyList") {
-    listID = "63ea106e843a53f2e4b457f3"
-  } else if (listName === "inventoryList") {
-    listID = "63ea107d843a53f2e4b457f4"
-  } else {
-    return console.error("ApiGet function need to know if it is 'buylist' or 'inventoryList'!")
-  }
-  const res = await fetch(
-    `https://nackademin-item-tracker.herokuapp.com/lists/${listID}`
-    );
-  const data = await res.json();
-  console.log(data.itemList) //TODO Change this line to a draw function
 }
 
 
