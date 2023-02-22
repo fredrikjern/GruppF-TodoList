d6c8e23ce6a6ecb84ccaecca8e547e5b4b8b0a6f
fredrikjern <fjernberg@gmail.com>
2023-02-22 14:45:53 +0100

    Co-authored-by: Martin <berra200@users.noreply.github.com> Co-authored-by: Nanthiporn Chongnoot <miamua@users.noreply.github.com>

--- main.js
+++ main.js
@@ -36,9 +36,7 @@ async function apiGet(listID) {
  * Post new objects to a list which is specified in the input
  * @param {* a string with the list ID} listID
  */
-async function apiPost(listID,title,description) {
-  
-
+async function apiPost(listID, title, description) {
   const res = await fetch(
     `https://nackademin-item-tracker.herokuapp.com/lists/${listID}/items`,
     {
@@ -56,8 +54,11 @@ async function apiPost(listID,title,description) {
   const data = await res.json();
   printToList(data.list, listID);
 }
-
-
+/** add Item
+ * @param {*} listansID 
+ * @param {*} inputVara 
+ * @param {*} inputAntal 
+ */
 async function addItemToList(listansID, inputVara, inputAntal) {
   const res = await fetch(
     `https://nackademin-item-tracker.herokuapp.com/lists/${listansID}/items`,
@@ -82,8 +83,6 @@ async function addItemToList(listansID, inputVara, inputAntal) {
  * @param {* A String} listName
  */
 function printToList(items, listName) {
-  // console.log(items);
-  // console.log(listName);
   if (listName === buyID) {
     list = "buy-list";
   } else if (listName === inventoryID) {
@@ -99,10 +98,9 @@ function printToList(items, listName) {
  * createItem
  * @param {*} obj
  * @param {*} list
- * @param {*} listIDs
+ * @param {*} listID
  */
-function createItem(obj, list, listIDs) {
-  // console.log(list);
+function createItem(obj, list, listID) {
   let liElem = document.createElement("li");
   liElem.innerHTML = `<p>${obj.title}, ${obj.description}</p>`;
 
@@ -112,9 +110,12 @@ function createItem(obj, list, listIDs) {
   let span = document.createElement("span");
   span.innerHTML = `<i class="fa-solid fa-carrot"></i>`;
   let checkbox = document.createElement("INPUT");
+  let objInput = [obj.title, obj.description, obj._id];
   checkbox.setAttribute("type", "checkbox");
   checkbox.setAttribute("name", `${list === "buy-list" ? "buy" : "inventory"}`);
   checkbox.setAttribute("value", `${obj._id}`);
+  checkbox.setAttribute("data-title", `${obj.title}`);
+  checkbox.setAttribute("data-description", `${obj.description}`);
   if (obj.checked === "true") checkbox.setAttribute("checked", "true");
   label.append(checkbox);
   label.append(span);
@@ -128,7 +129,8 @@ function createItem(obj, list, listIDs) {
 
   document.getElementById(list).append(liElem); // It's here the element is added to the DOM and eventlisteners can be added.
 
-  label.addEventListener("change", async function () {
+  label.addEventListener("click", async function (event) {
+    event.preventDefault();
     const res = await fetch(`${API_BASE}lists/${listID}/items/${obj._id}`, {
       method: "PUT",
       headers: {
@@ -138,71 +140,91 @@ function createItem(obj, list, listIDs) {
         checked: `${obj.checked === "false" ? "true" : "false"}`,
       }),
     });
-    apiGet(listID);
+    const data = await res.json();
+    apiGet(data.list._id);
   });
-  let listID = listIDs;
+
   deleteItemBtn.addEventListener("click", async function () {
     const res = await fetch(`${API_BASE}lists/${listID}/items/${obj._id}`, {
       method: "DELETE",
     }); // deletar objekt med _id.
-    // console.log(listID + "  klick på delete");
-
     apiGet(listID);
-    // printToList(data.list);
   });
 }
-//!-------------------
 // collect allt checked checkboxes in one list, post copied item, then delete old item.
-function transferItems(listname) {
-  let checkboxes = document.querySelectorAll(`[name='${listname}']`);
-  console.log(checkboxes);
-  checkboxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-
-      //let newTitle =checkbox.title
-      //let newDesc=checkbox.desc
-      // delete på (checkbox.value)ID
-    }
-    //console.log(checkbox);
+async function deleteItem(listID, objectID) {
+  return fetch(`${API_BASE}lists/${listID}/items/${objectID}`, {
+    method: "DELETE",
   });
 }
+/**
+ * @param {*} listname
+ */
+async function transferItems(listname) {
+  let checkboxes = document.querySelectorAll(`[name='${listname}']`);
+  const allChecked = Array.from(checkboxes).filter(
+    (checkbox) => checkbox.checked
+  );
+  let howManyAreDone = 0;
+  allChecked.forEach(async (checkbox, i) => {
+    if (checkbox.checked) {
+      addItemToList(
+        listname === "buy" ? inventoryID : buyID,
+        checkbox.dataset.title,
+        checkbox.dataset.description
+      );
 
+      await deleteItem(
+        listname === "buy" ? buyID : inventoryID,
+        checkbox.value
+      );
+
+      howManyAreDone++;
+
+      if (howManyAreDone === allChecked.length) {
+        apiGet(buyID);
+      }
+    }
+  });
+}
+/**
+ * @param {*} inputMain
+ * @param {*} inputDesc
+ */
 const errorMessage = (inputMain, inputDesc) => {
   if (!inputMain.trim().length || !inputDesc.trim().length) {
     alert("Du måste skriva något i båda fälten.");
     throw new Error("Input måste innehålla minst en karaktär.");
   }
-}
-
-//
-/**
+};
+/** Eventlistener
  * Eventlisteners for forms, event submit
  */
 shoppingField.addEventListener("submit", function (e) {
   e.preventDefault();
-  
-    let inputMain = document.querySelector("#shoppingField").value;
-    let inputDesc = document.querySelector("#shoppingDesc").value;
-    errorMessage(inputMain, inputDesc)
-    shoppingField.reset();
-  
-    apiPost(buyID, inputMain, inputDesc);
+
+  let inputMain = document.querySelector("#shoppingField").value;
+  let inputDesc = document.querySelector("#shoppingDesc").value;
+  errorMessage(inputMain, inputDesc);
+  shoppingField.reset();
+
+  apiPost(buyID, inputMain, inputDesc);
 });
 homeField.addEventListener("submit", function (e) {
   e.preventDefault();
-   let inputMain = document.querySelector("#homeField").value;
-   let inputDesc = document.querySelector("#homeDesc").value;
-   errorMessage(inputMain, inputDesc)
-   homeField.reset();
+  let inputMain = document.querySelector("#homeField").value;
+  let inputDesc = document.querySelector("#homeDesc").value;
+  errorMessage(inputMain, inputDesc);
+  homeField.reset();
   apiPost(inventoryID, inputMain, inputDesc);
 });
 
 shoppingMove.addEventListener("click", function (e) {
-  transferItems("buy")
+  transferItems("buy");
 });
 
 inventoryMove.addEventListener("click", function (e) {
-  transferItems("inventory")
+  transferItems("inventory");
 });
 
 /**
